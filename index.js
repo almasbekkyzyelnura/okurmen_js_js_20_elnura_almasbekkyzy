ce114f3c2ecf3f24c69a223a048878aefa3a64be
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-04-03 15:17:27 +0600

    js

---
+++ index.js
@@ -0,0 +1,80 @@
+
+ //foreach
+ const arr2 = ['a', 'u', 'p'];
+const arr = [1, 3, 4, 7, 12, ];
+//arr.forEach(item => console.log(item));
+
+/*
+//map
+arr.map((el,idx) => console.log('index', idx * '' , el * 2));
+
+
+
+
+//filter
+const Array = arr.filter(item => item % 2 === 0);
+console.log(Array);
+
+
+//every
+ //let isValid = (value) => value > 0;
+//console.log('isValid', arr.every(isValid));
+
+
+
+//find
+const arr6 = arr.find((el) => el > 10);
+console.log(arr6);
+
+
+//findIndex
+let index = arr.findIndex((el) => el < 0);
+console.log(index);
+
+
+// concat
+const arr4 = arr.concat(arr2);
+console.log(arr4);
+const a = arr.entries();
+*/
+
+
+
+//fill
+const arr5 = new Array(5).fill(0);
+console.log(arr5);
+
+/*
+// findLast
+const arr7 = arr.findLast((el) => el < 5);
+console.log(arr7);
+
+
+
+//  findLastIndex
+const arr8 = arr.reverse().findIndex(element => element === 0);
+console.log(arr8);
+
+
+
+
+// flatMap
+const arr9 = arr.flatMap(item => [item * 2]);
+console.log(arr9);
+
+
+
+//entries
+const arr10 = arr.entries();
+console.log(arr10);
+
+
+
+//includes
+
+console.log(arr.includes(5));
+
+
+*/
+
+
